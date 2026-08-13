bb45fbc32259cd1f6a3d87cb914ad4a8b99217fd
adithyanmarikkal <adithyanmunnikrishnan@gmail.com>
2026-08-13 23:27:55 +0530

    backend render

--- myapp/vite.config.js
+++ myapp/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: ['http://localhost:8000', 'https://opti-more.onrender.com'],
         changeOrigin: true,
       },
     },
